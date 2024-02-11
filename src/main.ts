a5a1e0150953785dea0ee4a42dd15a5cc4d2bc57
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-02-11 17:36:54 +0900

    release: 0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,153 @@
-import { Plugin } from 'obsidian';
-import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from 'settings';
+import { EditableFileView, Plugin, TFile, View, WorkspaceLeaf, normalizePath } from 'obsidian';
+import { ViewSyncSettings, DEFAULT_SETTINGS, ViewSyncSettingTab } from 'settings';
 
+
+declare module 'obsidian' {
+	interface App {
+		loadLocalStorage(key: string): string | null;
+		// - If the second argument is not provided, it will remove the key from the local storage.
+		// - `value` can be anything that can be serialized to JSON, but be careful that values
+		//   that become false when casted to boolean will cause the key being removed from the local storage.
+		saveLocalStorage(key: string, value?: any): void;
+	}
+
+	interface Workspace {
+		on(name: string, callback: (...args: any[]) => any, ctx?: any): EventRef;
+		on(name: 'view-sync:state-change', callback: (view: View) => any, ctx?: any): EventRef;
+	}
+
+	interface WorkspaceLeaf {
+		isVisible(): boolean;
+	}
+}
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: ViewSyncSettings;
 
-	async onload() {
-		await this.loadSettings();
-		await this.saveSettings();
-		this.addSettingTab(new SampleSettingTab(this));
+	onload() {
+		this.loadSettings();
+		this.saveSettings();
+		this.addSettingTab(new ViewSyncSettingTab(this));
+
+		this.registerViewSyncEventPublisher();
+		this.registerViewSyncEventSubscriber();
+
+		this.registerWorkspaceSyncEventPublisher();
+		this.registerWorkspaceSyncEventSubscriber();
+
+		this.registerFileRenameHandler();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	get loadStrorageKey() {
+		return this.manifest.id + '-settings';
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, this.app.loadLocalStorage(this.loadStrorageKey));
+	}
+
+	saveSettings() {
+		this.app.saveLocalStorage(this.loadStrorageKey, this.settings);
+	}
+
+	async onViewStateChange(view: View) {
+		const path = normalizePath(this.settings.ownPath);
+		if (!path) return;
+
+		if (this.settings.viewTypes.contains(view.getViewType())) {
+			const leaf = view.leaf;
+			if (leaf !== this.app.workspace.activeLeaf) return;
+
+			const viewState = leaf.getViewState();
+			const serialized = JSON.stringify(viewState);
+			await this.writeFile(path, serialized);
+		}
+	}
+
+	onWorkspaceLayoutChange() {
+		const path = normalizePath(this.settings.ownWorkspacePath);
+		if (!path) return;
+
+		const layout = this.app.workspace.getLayout();
+		const serialized = JSON.stringify(layout);
+		this.writeFile(path, serialized);
+	}
+
+	registerViewSyncEventPublisher() {
+		this.registerEvent(this.app.workspace.on('view-sync:state-change', (view) => {
+			this.onViewStateChange(view);
+			this.onWorkspaceLayoutChange();
+		}));
+
+		this.registerEvent(this.app.workspace.on('active-leaf-change', async (leaf) => {
+			if (leaf) await this.onViewStateChange(leaf.view);
+		}));
+	}
+
+	registerViewSyncEventSubscriber() {
+		this.registerEvent(this.app.vault.on('modify', async (file) => {
+			if (this.settings.watchAnotherWorkspace) return;
+
+			if (this.settings.watchAnother && file instanceof TFile && normalizePath(this.settings.watchPath) === file.path) {
+				const data = await this.app.vault.read(file);
+				const viewState = JSON.parse(data);
+				let leaf: WorkspaceLeaf | undefined;
+				const activeLeaf = this.app.workspace.activeLeaf;
+				if (activeLeaf && activeLeaf.getRoot() === this.app.workspace.rootSplit) {
+					leaf = activeLeaf;
+				} else {
+					this.app.workspace.iterateRootLeaves((l) => {
+						if (!leaf && l.isVisible())
+							leaf = l;
+					});
+				}
+				if (leaf) {
+					leaf.setViewState(viewState);
+				}
+			}
+		}));
+	}
+
+	registerWorkspaceSyncEventPublisher() {
+		this.registerEvent(this.app.workspace.on('layout-change', this.onWorkspaceLayoutChange, this));
+	}
+
+	registerWorkspaceSyncEventSubscriber() {
+		this.registerEvent(this.app.vault.on('modify', async (file) => {
+			if (this.settings.watchAnotherWorkspace && file instanceof TFile && normalizePath(this.settings.watchWorkspacePath) === file.path) {
+				const data = await this.app.vault.read(file);
+				const layout = JSON.parse(data);
+				this.app.workspace.changeLayout(layout);
+			}
+		}));
+	}
+
+	async writeFile(normalizedPath: string, data: string) {
+		const file = this.app.vault.getAbstractFileByPath(normalizedPath);
+
+		if (file instanceof TFile) {
+			return await this.app.vault.modify(file, data);
+		}
+
+		const folderPath = normalizePath(normalizedPath.split('/').slice(0, -1).join('/'));
+		if (folderPath) {
+			const folderExists = !!(this.app.vault.getAbstractFileByPath(folderPath));
+			if (!folderExists) {
+				await this.app.vault.createFolder(folderPath);
+			}
+		}
+
+		if (normalizedPath) {
+			return await this.app.vault.create(normalizedPath, data);
+		}
+	}
+
+	registerFileRenameHandler() {
+		this.registerEvent(this.app.vault.on('rename', (file, oldPath) => {
+			if (oldPath === normalizePath(this.settings.ownPath)) {
+				this.settings.ownPath = file.path;
+				this.saveSettings();
+			}
+		}));
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,54 +1,142 @@
-import { PluginSettingTab, Setting } from 'obsidian';
+import { AbstractInputSuggest, Component, IconName, MarkdownRenderer, PluginSettingTab, SearchResultContainer, Setting, TFile, prepareFuzzySearch, renderResults, setIcon, setTooltip, sortSearchResults } from 'obsidian';
 import MyPlugin from 'main';
 
 
-export interface MyPluginSettings {
+export interface ViewSyncSettings {
+	ownPath: string;
+	viewTypes: string[];
+	watchAnother: boolean;
+	watchPath: string;
+	ownWorkspacePath: string;
+	watchAnotherWorkspace: boolean;
+	watchWorkspacePath: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: ViewSyncSettings = {
+	ownPath: '',
+	viewTypes: ['pdf'],
+	watchAnother: false,
+	watchPath: '',
+	ownWorkspacePath: '',
+	watchAnotherWorkspace: false,
+	watchWorkspacePath: '',
 };
 
 // Inspired by https://stackoverflow.com/a/50851710/13613783
 export type KeysOfType<Obj, Type> = NonNullable<{ [k in keyof Obj]: Obj[k] extends Type ? k : never }[keyof Obj]>;
 
-export class SampleSettingTab extends PluginSettingTab {
+export class ViewSyncSettingTab extends PluginSettingTab {
+	component: Component;
+	items: Partial<Record<keyof ViewSyncSettings, Setting>>;
+	promises: Promise<any>[];
+
 	constructor(public plugin: MyPlugin) {
 		super(plugin.app, plugin);
+		this.component = new Component();
+		this.items = {};
+		this.promises = [];
 	}
 
-	addHeading(heading: string) {
-	    return new Setting(this.containerEl).setName(heading).setHeading();
-    }
+	get settings(): ViewSyncSettings {
+		return this.plugin.settings;
+	}
 
-	addTextSetting(settingName: KeysOfType<MyPluginSettings, string>) {
-		return new Setting(this.containerEl)
+	addSetting(settingName?: keyof ViewSyncSettings) {
+		const item = new Setting(this.containerEl);
+		if (settingName) this.items[settingName] = item;
+		return item;
+	}
+
+	scrollTo(settingName: keyof ViewSyncSettings) {
+		this.items[settingName]?.settingEl.scrollIntoView();
+	}
+
+	addHeading(heading: string, icon?: IconName) {
+		return this.addSetting()
+			.setName(heading)
+			.setHeading()
+			.then((setting) => {
+				if (icon) {
+					const iconEl = createDiv();
+					setting.settingEl.prepend(iconEl)
+					setIcon(iconEl, icon);
+				}
+			});
+	}
+
+	addPathSetting(settingName: KeysOfType<ViewSyncSettings, string>, placeholder?: string) {
+		let newPath = this.settings[settingName];
+
+		this.component.register(() => {
+			const oldPath = this.settings[settingName];
+			this.settings[settingName] = newPath;
+			if (oldPath !== newPath) {
+				this.plugin.saveSettings();
+				const file = this.app.vault.getAbstractFileByPath(oldPath);
+				if (file instanceof TFile) {
+					this.app.fileManager.renameFile(file, newPath);
+				}
+			}
+		});
+
+		return this.addSetting(settingName)
 			.addText((text) => {
-				text.setValue(this.plugin.settings[settingName])
-					.setPlaceholder(DEFAULT_SETTINGS[settingName])
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
+				text.setValue(this.settings[settingName])
+					.setPlaceholder(placeholder ?? '')
+					.onChange((value) => {
+						newPath = value;
 					});
 			});
 	}
 
-	addNumberSetting(settingName: KeysOfType<MyPluginSettings, number>) {
-		return new Setting(this.containerEl)
+	addCSVSetting(settingName: KeysOfType<ViewSyncSettings, string[]>, placeholder?: string, onBlur?: () => any) {
+		return this.addSetting(settingName)
 			.addText((text) => {
-				text.setValue('' + this.plugin.settings[settingName])
-					.setPlaceholder('' + DEFAULT_SETTINGS[settingName])
-					.then((text) => text.inputEl.type = "number")
+				text.setValue(this.plugin.settings[settingName].join(', '))
+					.setPlaceholder(placeholder ?? '')
+					.then((text) => {
+						if (placeholder) {
+							text.inputEl.size = Math.max(text.inputEl.size, text.inputEl.placeholder.length);
+						}
+					})
 					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value === '' ? DEFAULT_SETTINGS[settingName] : +value;
-						await this.plugin.saveSettings();
+						this.plugin.settings[settingName] = value.split(',').map((s) => s.trim());
+						this.plugin.saveSettings();
 					});
+				if (onBlur) this.component.registerDomEvent(text.inputEl, 'blur', onBlur);
 			});
 	}
 
-	addToggleSetting(settingName: KeysOfType<MyPluginSettings, boolean>, extraOnChange?: (value: boolean) => void) {
-		return new Setting(this.containerEl)
+	// addTextAreaSetting(settingName: KeysOfType<ViewSyncSettings, string>, placeholder?: string, onBlur?: () => any) {
+	// 	return this.addSetting(settingName)
+	// 		.addTextArea((text) => {
+	// 			text.setValue(this.plugin.settings[settingName])
+	// 				.setPlaceholder(placeholder ?? '')
+	// 				.onChange(async (value) => {
+	// 					// @ts-ignore
+	// 					this.plugin.settings[settingName] = value;
+	// 					await this.plugin.saveSettings();
+	// 				});
+	// 			if (onBlur) this.component.registerDomEvent(text.inputEl, 'blur', onBlur);
+	// 		});
+	// }
+
+	// addNumberSetting(settingName: KeysOfType<ViewSyncSettings, number>) {
+	// 	return this.addSetting(settingName)
+	// 		.addText((text) => {
+	// 			text.setValue('' + this.plugin.settings[settingName])
+	// 				.setPlaceholder('' + DEFAULT_SETTINGS[settingName])
+	// 				.then((text) => text.inputEl.type = 'number')
+	// 				.onChange(async (value) => {
+	// 					// @ts-ignore
+	// 					this.plugin.settings[settingName] = value === '' ? DEFAULT_SETTINGS[settingName] : +value;
+	// 					await this.plugin.saveSettings();
+	// 				});
+	// 		});
+	// }
+
+	addToggleSetting(settingName: KeysOfType<ViewSyncSettings, boolean>, extraOnChange?: (value: boolean) => void) {
+		return this.addSetting(settingName)
 			.addToggle((toggle) => {
 				toggle.setValue(this.plugin.settings[settingName])
 					.onChange(async (value) => {
@@ -60,42 +148,349 @@ export class SampleSettingTab extends PluginSettingTab {
 			});
 	}
 
-	addDropdowenSetting(settingName: KeysOfType<MyPluginSettings, string>, options: readonly string[], display?: (option: string) => string, extraOnChange?: (value: string) => void) {
-		return new Setting(this.containerEl)
-			.addDropdown((dropdown) => {
-				const displayNames = new Set<string>();
-				for (const option of options) {
-					const displayName = display?.(option) ?? option;
-					if (!displayNames.has(displayName)) {
-						dropdown.addOption(option, displayName);
-						displayNames.add(displayName);
-					}
-				};
-				dropdown.setValue(this.plugin.settings[settingName])
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
-						extraOnChange?.(value);
+	// addColorPickerSetting(settingName: KeysOfType<ViewSyncSettings, HexString>, extraOnChange?: (value: HexString) => void) {
+	// 	return this.addSetting(settingName)
+	// 		.addColorPicker((picker) => {
+	// 			picker.setValue(this.plugin.settings[settingName])
+	// 				.onChange(async (value) => {
+	// 					// @ts-ignore
+	// 					this.plugin.settings[settingName] = value;
+	// 					await this.plugin.saveSettings();
+	// 					extraOnChange?.(value);
+	// 				});
+	// 		});
+	// }
+
+	// addDropdownSetting(settingName: KeysOfType<ViewSyncSettings, string>, options: readonly string[], display?: (option: string) => string, extraOnChange?: (value: string) => void): Setting;
+	// addDropdownSetting(settingName: KeysOfType<ViewSyncSettings, string>, options: Record<string, string>, extraOnChange?: (value: string) => void): Setting;
+	// addDropdownSetting(settingName: KeysOfType<ViewSyncSettings, string>, ...args: any[]) {
+	// 	let options: string[] = [];
+	// 	let display = (optionValue: string) => optionValue;
+	// 	let extraOnChange = (value: string) => { };
+	// 	if (Array.isArray(args[0])) {
+	// 		options = args[0];
+	// 		if (typeof args[1] === 'function') display = args[1];
+	// 		if (typeof args[2] === 'function') extraOnChange = args[2];
+	// 	} else {
+	// 		options = Object.keys(args[0]);
+	// 		display = (optionValue: string) => args[0][optionValue];
+	// 		if (typeof args[1] === 'function') extraOnChange = args[1];
+	// 	}
+	// 	return this.addSetting(settingName)
+	// 		.addDropdown((dropdown) => {
+	// 			for (const option of options) {
+	// 				const displayName = display(option) ?? option;
+	// 				dropdown.addOption(option, displayName);
+	// 			}
+	// 			dropdown.setValue(this.plugin.settings[settingName])
+	// 				.onChange(async (value) => {
+	// 					// @ts-ignore
+	// 					this.plugin.settings[settingName] = value;
+	// 					await this.plugin.saveSettings();
+	// 					extraOnChange?.(value);
+	// 				});
+	// 		});
+	// }
+
+	// addIndexDropdownSetting(settingName: KeysOfType<ViewSyncSettings, number>, options: readonly string[], display?: (option: string) => string, extraOnChange?: (value: number) => void): Setting {
+	// 	return this.addSetting(settingName)
+	// 		.addDropdown((dropdown) => {
+	// 			for (const option of options) {
+	// 				const displayName = display?.(option) ?? option;
+	// 				dropdown.addOption(option, displayName);
+	// 			}
+	// 			const index = this.plugin.settings[settingName];
+	// 			const option = options[index];
+	// 			dropdown.setValue(option)
+	// 				.onChange(async (value) => {
+	// 					const newIndex = options.indexOf(value);
+	// 					if (newIndex !== -1) {
+	// 						// @ts-ignore
+	// 						this.plugin.settings[settingName] = newIndex;
+	// 						await this.plugin.saveSettings();
+	// 						extraOnChange?.(newIndex);
+	// 					}
+	// 				});
+	// 		});
+	// }
+
+	// addSliderSetting(settingName: KeysOfType<ViewSyncSettings, number>, min: number, max: number, step: number) {
+	// 	return this.addSetting(settingName)
+	// 		.addSlider((slider) => {
+	// 			slider.setLimits(min, max, step)
+	// 				.setValue(this.plugin.settings[settingName])
+	// 				.setDynamicTooltip()
+	// 				.onChange(async (value) => {
+	// 					// @ts-ignore
+	// 					this.plugin.settings[settingName] = value;
+	// 					await this.plugin.saveSettings();
+	// 				});
+	// 		});
+	// }
+
+	addDesc(desc: string) {
+		return this.addSetting()
+			.setDesc(desc);
+	}
+
+	addFundingButton() {
+		return this.addHeading('Support development', 'lucide-heart')
+			.setDesc(`If you find ${this.plugin.manifest.name} helpful, please consider supporting the development to help me keep this plugin alive.\n\nIf you prefer PayPal, please make donations via Ko-fi. Thank you!`)
+			.then((setting) => {
+				const infoEl = setting.infoEl;
+				const iconEl = setting.settingEl.firstElementChild;
+				if (!iconEl) return;
+
+				const container = setting.settingEl.createDiv();
+				container.appendChild(iconEl);
+				container.appendChild(infoEl);
+				setting.settingEl.prepend(container);
+
+				setting.settingEl.id = `${this.plugin.manifest.id}-funding`;
+				container.id = `${this.plugin.manifest.id}-funding-icon-info-container`;
+				iconEl.id = `${this.plugin.manifest.id}-funding-icon`;
+			})
+			.addButton((button) => {
+				button
+					.setButtonText('GitHub Sponsors')
+					.onClick(() => {
+						open('https://github.com/sponsors/RyotaUshio');
+					});
+			})
+			.addButton((button) => {
+				button
+					.setButtonText('Buy Me a Coffee')
+					.onClick(() => {
+						open('https://www.buymeacoffee.com/ryotaushio');
+					});
+			})
+			.addButton((button) => {
+				button
+					.setButtonText('Ko-fi')
+					.onClick(() => {
+						open('https://ko-fi.com/ryotaushio');
 					});
 			});
 	}
 
-	addSliderSetting(settingName: KeysOfType<MyPluginSettings, number>, min: number, max: number, step: number) {
-		return new Setting(this.containerEl)
-			.addSlider((slider) => {
-				slider.setLimits(min, max, step)
-					.setValue(this.plugin.settings[settingName])
-					.setDynamicTooltip()
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
-					});
-			});
+	async renderMarkdown(lines: string[] | string, el: HTMLElement) {
+		this.promises.push(this._renderMarkdown(lines, el));
+		el.addClass('markdown-rendered');
 	}
-	
-	display(): void {
+
+	async _renderMarkdown(lines: string[] | string, el: HTMLElement) {
+		await MarkdownRenderer.render(this.app, Array.isArray(lines) ? lines.join('\n') : lines, el, '', this.component);
+		if (el.childNodes.length === 1 && el.firstChild instanceof HTMLParagraphElement) {
+			el.replaceChildren(...el.firstChild.childNodes);
+		}
+	}
+
+	// addNameValuePairListSetting<Item>(items: Item[], index: number, defaultIndexKey: KeysOfType<ViewSyncSettings, number>, accesors: {
+	// 	getName: (item: Item) => string,
+	// 	setName: (item: Item, value: string) => void,
+	// 	getValue: (item: Item) => string,
+	// 	setValue: (item: Item, value: string) => void,
+	// }, configs: {
+	// 	name: {
+	// 		placeholder: string,
+	// 		formSize: number,
+	// 		duplicateMessage: string,
+	// 	},
+	// 	value: {
+	// 		placeholder: string,
+	// 		formSize: number,
+	// 		formRows?: number, // for multi-line value
+	// 	},
+	// 	delete: {
+	// 		deleteLastMessage: string,
+	// 	}
+	// }) {
+	// 	const { getName, setName, getValue, setValue } = accesors;
+	// 	const item = items[index];
+	// 	const name = getName(item);
+	// 	const value = getValue(item);
+
+	// 	return this.addSetting()
+	// 		.addText((text) => {
+	// 			text.setPlaceholder(configs.name.placeholder)
+	// 				.then((text) => {
+	// 					text.inputEl.size = configs.name.formSize;
+	// 					setTooltip(text.inputEl, configs.name.placeholder);
+	// 				})
+	// 				.setValue(name)
+	// 				.onChange(async (newName) => {
+	// 					if (items.some((item) => getName(item) === newName)) {
+	// 						new Notice(configs.name.duplicateMessage);
+	// 						text.inputEl.addClass('error');
+	// 						return;
+	// 					}
+	// 					text.inputEl.removeClass('error');
+	// 					setName(item, newName);
+
+	// 					const setting = this.items[defaultIndexKey];
+	// 					if (setting) {
+	// 						const optionEl = ((setting as Setting).components[0] as DropdownComponent).selectEl.querySelector<HTMLOptionElement>(`:scope > option:nth-child(${index + 1})`);
+	// 						if (optionEl) {
+	// 							optionEl.value = newName;
+	// 							optionEl.textContent = newName;
+	// 						}
+	// 					}
+
+	// 					await this.plugin.saveSettings();
+	// 				});
+	// 		})
+	// 		.then((setting) => {
+	// 			if (configs.value.hasOwnProperty('formRows')) {
+	// 				setting.addTextArea((textarea) => {
+	// 					textarea.setPlaceholder(configs.value.placeholder)
+	// 						.then((textarea) => {
+	// 							textarea.inputEl.rows = configs.value.formRows!;
+	// 							textarea.inputEl.cols = configs.value.formSize;
+	// 							setTooltip(textarea.inputEl, configs.value.placeholder);
+	// 						})
+	// 						.setValue(value)
+	// 						.onChange(async (newValue) => {
+	// 							setValue(item, newValue);
+	// 							await this.plugin.saveSettings();
+	// 						});
+	// 				});
+	// 			} else {
+	// 				setting.addText((textarea) => {
+	// 					textarea.setPlaceholder(configs.value.placeholder)
+	// 						.then((text) => {
+	// 							text.inputEl.size = configs.value.formSize;
+	// 							setTooltip(text.inputEl, configs.value.placeholder);
+	// 						})
+	// 						.setValue(value)
+	// 						.onChange(async (newValue) => {
+	// 							setValue(item, newValue);
+	// 							await this.plugin.saveSettings();
+	// 						});
+	// 				})
+	// 			}
+	// 		})
+	// 		.addExtraButton((button) => {
+	// 			button.setIcon('trash')
+	// 				.setTooltip('Delete')
+	// 				.onClick(async () => {
+	// 					if (items.length === 1) {
+	// 						new Notice(configs.delete.deleteLastMessage);
+	// 						return;
+	// 					}
+	// 					items.splice(index, 1);
+	// 					if (this.plugin.settings[defaultIndexKey] >= index) {
+	// 						this.plugin.settings[defaultIndexKey]--;
+	// 					}
+	// 					await this.plugin.saveSettings();
+	// 					this.redisplay();
+	// 				});
+	// 		})
+	// 		.setClass('no-border');
+	// }
+
+	addHotkeySettingButton(setting: Setting) {
+		setting.addButton((button) => {
+			button.setButtonText('Open hotkeys settings')
+				.onClick(() => {
+					// @ts-ignore
+					const tab = this.app.setting.openTabById('hotkeys');
+					tab.setQuery(this.plugin.manifest.id);
+				});
+		});
+	}
+
+	/** Refresh the setting tab and then scroll back to the original position. */
+	async redisplay() {
+		const scrollTop = this.containerEl.scrollTop;
+		this.display();
+		this.containerEl.scroll({ top: scrollTop });
+	}
+
+	display() {
+		this.component.unload();
+		this.component.load();
 		this.containerEl.empty();
+
+		this.addHeading('View sync', 'lucide-file-text');
+
+		this.addPathSetting('ownPath', 'view-sync.json')
+			.setName('Own path');
+		this.addCSVSetting('viewTypes', 'ex) markdown, pdf, canvas')
+			.setName('View types to watch');
+		this.addToggleSetting('watchAnother', () => this.redisplay())
+			.setName('Follow another device');
+
+		if (this.settings.watchAnother) {
+			this.addPathSetting('watchPath')
+				.setName('File path to watch');
+		}
+
+		this.addHeading('Workspace sync', 'lucide-layout');
+
+		this.addPathSetting('ownWorkspacePath', 'workspace-sync.json')
+			.setName('Own path');
+		this.addToggleSetting('watchAnotherWorkspace', () => this.redisplay())
+			.setName('Follow another device');
+
+		if (this.settings.watchAnotherWorkspace) {
+			this.addPathSetting('watchWorkspacePath')
+				.setName('File path to watch');
+		}
+
+		this.addFundingButton();
+	}
+
+	hide() {
+		this.component.unload();
+	}
+}
+
+
+type Ranked<Item> = SearchResultContainer & { item: Item };
+
+abstract class FuzzyInputSuggest<Item> extends AbstractInputSuggest<Ranked<Item>> {
+	plugin: MyPlugin;
+	inputEl: HTMLInputElement;
+
+	constructor(plugin: MyPlugin, inputEl: HTMLInputElement) {
+		super(plugin.app, inputEl);
+		this.inputEl = inputEl;
+		this.plugin = plugin;
+	}
+
+	abstract getItems(): Item[];
+
+	abstract getItemText(item: Item): string;
+
+	abstract getName(item: Item): string;
+
+	getSuggestions(query: string) {
+		const search = prepareFuzzySearch(query.trim());
+		const items = this.getItems();
+
+		const results: Ranked<Item>[] = [];
+
+		for (const item of items) {
+			const target = this.getItemText(item);
+			const match = search(target);
+			if (match) results.push({ match, item });
+		}
+
+		sortSearchResults(results);
+
+		return results;
+	}
+
+	renderSuggestion(rankedItem: Ranked<Item>, el: HTMLElement) {
+		const { item, match } = rankedItem;
+		renderResults(el, this.getItemText(item), match);
+	}
+
+	selectSuggestion(rankedItem: Ranked<Item>) {
+		const { item } = rankedItem;
+
+		this.inputEl.blur();
+		this.inputEl.value = this.getName(item);
+		this.close();
 	}
 }
